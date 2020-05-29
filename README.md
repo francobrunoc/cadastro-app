a9292d8fb2a51d4074e31e8d715352d1f0635cd0
franco <francobrunoc@gmail.com>
2020-05-28 22:30:17 -0300

    Readme complete instructions and dist directory.

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # cadastro-app-vue
 
+Single page application for handling of cadastro-api.
+
+You will need some sort of static HTTP server to run (the dist directory). You could use http-server, Webpack, Browserify or other.
+
+### http-server installation
+```
+npm install -g http-server
+```
+### running on http-server
+```
+cd cadastro-app/dist
+http-server
+```
+
 ## Project setup
 ```
 npm install
---
+++ dist/js/about.6f5c9c4e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{"19f6":function(t,e,n){t.exports=n.p+"img/its-alive.68b5a1a2.jpeg"},f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},i=[function(){var t=this,e=t.$createElement,a=t._self._c||e;return a("div",{staticClass:"about"},[a("img",{attrs:{src:n("19f6"),height:"238",width:"300"}})])}],s=n("2877"),r={},c=Object(s["a"])(r,a,i,!1,null,null,null);e["default"]=c.exports}}]);
+//# sourceMappingURL=about.6f5c9c4e.js.map
